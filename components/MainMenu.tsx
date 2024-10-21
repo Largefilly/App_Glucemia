8dc2dd7cd8d59cbcbf3a2cb5c63e086aa0de5c67
largefilly <jorge.tarapa.1702@hotmail.com>
2024-10-20 19:01:47 -0500

    Create MainMenu.tsx

---
+++ components/MainMenu.tsx
@@ -0,0 +1,26 @@
+// Components/MainMenuScreen.tsx
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const MainMenuScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Menu Principal WAAA</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default MainMenuScreen;
